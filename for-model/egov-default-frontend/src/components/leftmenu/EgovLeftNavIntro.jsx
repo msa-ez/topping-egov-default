5e54160f7287119dcea225a973040cd01046e02f
kyusoo <rbtn110@uengine.org>
2024-04-27 15:59:05 +0900

    gitpod test를 통해 수정된 사항 반영

--- for-model/egov-default-frontend/src/components/leftmenu/EgovLeftNavIntro.jsx
+++ for-model/egov-default-frontend/src/components/leftmenu/EgovLeftNavIntro.jsx
@@ -1,21 +1,32 @@
+forEach: BoundedContext
+fileName: EgovLeftNavIntro.jsx
+path: egov-default-frontend/src/leftmenu
+---
 import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 import URL from 'constants/url';
 
 function EgovLeftNavIntro() {
+
+    const lastSegment = window.parent.location.href.split('/').pop();
     
-    return (
-        <div className="nav">
-            <div className="inner">
-                <h2>정보마당</h2>
-                <ul className="menu4">
-                    <li><NavLink to={URL.INTRO_WORKS} className={({ isActive }) => (isActive ? "cur" : "")}>주요사업 소개</NavLink></li>
-                    <li><NavLink to={URL.INTRO_SERVICE} className={({ isActive }) => (isActive ? "cur" : "")}>대표서비스 소개</NavLink></li>
-                </ul>
+    {{}}
+    if (lastSegment === 'orders'){
+        return (
+            <div className="nav">
+                <div className="inner">
+                    <h2>알림마당</h2>
+                    <ul className="menu4">
+                        <li><NavLink to={URL.INFORM_DAILY} className={({ isActive }) => (isActive ? "cur" : "")}>오늘의행사</NavLink></li>
+                        <li><NavLink to={URL.INFORM_WEEKLY} className={({ isActive }) => (isActive ? "cur" : "")}>금주의행사</NavLink></li>
+                        <li><NavLink to={URL.INFORM_NOTICE} className={({ isActive }) => (isActive ? "cur" : "")}>공지사항</NavLink></li>
+                        <li><NavLink to={URL.INFORM_GALLERY} className={({ isActive }) => (isActive ? "cur" : "")}>사이트갤러리</NavLink></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default EgovLeftNavIntro;
