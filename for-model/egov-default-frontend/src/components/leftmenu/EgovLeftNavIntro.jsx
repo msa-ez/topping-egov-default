c68551508918ec242a185e8bb1826b0143a9fa0a
kyusoo <rbtn110@uengine.org>
2024-04-29 11:14:30 +0900

    egovLeftNavIntro 원복

--- for-model/egov-default-frontend/src/components/leftmenu/EgovLeftNavIntro.jsx
+++ for-model/egov-default-frontend/src/components/leftmenu/EgovLeftNavIntro.jsx
@@ -1,7 +1,3 @@
-forEach: Model
-fileName: EgovLeftNavIntro.jsx
-path: egov-default-frontend/src/components/leftmenu
----
 import React from 'react';
 
 import { NavLink } from 'react-router-dom';
@@ -9,25 +5,17 @@ import URL from 'constants/url';
 
 function EgovLeftNavIntro() {
 
-    const lastSegment = window.parent.location.href.split('/').pop();
-    
-    {{#boundedContexts}}
-    {{#aggregates}}
-    if (lastSegment === '{{namePlural}}'){
-        return (
-            <div className="nav">
-                <div className="inner">
-                    <h2>{{../namePascalCase}}</h2>
-                    <ul className="menu4">
-                        <li><NavLink to="/{{../nameCamelCase}}/{{namePlural}}" className={({ isActive }) => (isActive ? "cur" : "")}>{{#ifNotNull displayName namePascalCase}}{{/ifNotNull}}</NavLink></li>
-                    </ul>
-                </div>
+    return (
+        <div className="nav">
+            <div className="inner">
+                <h2>정보마당</h2>
+                <ul className="menu4">
+                    <li><NavLink to={URL.INTRO_WORKS} className={({ isActive }) => (isActive ? "cur" : "")}>주요사업 소개</NavLink></li>
+                    <li><NavLink to={URL.INTRO_SERVICE} className={({ isActive }) => (isActive ? "cur" : "")}>대표서비스 소개</NavLink></li>
+                </ul>
             </div>
-        );
-    }
-    {{/aggregates}}
-    {{/boundedContexts}}
-    return null;
+        </div>
+    );
 }
 
 export default EgovLeftNavIntro;
